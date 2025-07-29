f522e044673d3569f40a032e3552d5988513f2de
Ryan Waltemate <waltemry@oregonstate.edu>
2025-07-29 00:17:44 -0700

    Added another text to screen, change brightness level loop

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from PIL import ImageDraw, Image, ImageFont
 import time
 import RPi.GPIO as GPIO
 
-buspeed = 52 * 1000000
-
-serial = spi(port=0, device=0, gpio_DC=25, gpio_RST=27, bus_speed_hz=buspeed)
+serial = spi(port=0, device=0, gpio_DC=25, gpio_RST=27, bus_speed_hz=52000000)
 device = st7789(serial, width=320, height=240, rotate=0)
 
 
@@ -15,7 +13,8 @@ img = Image.new("RGB", device.size, "black")
 font = ImageFont.truetype("assets/Sans.ttf", 28)
 draw = ImageDraw.Draw(img)
 draw.rectangle((0, 0, 319, 239), fill="white")
-draw.text((0, 0), "Hello DietPi", font=font, fill="black")
+draw.text((1, 1), "Welcome DietPi", font=font, fill="black")
+draw.text((1, 30), "Play Shit", font=font, fill="black")
 
 
 
@@ -26,13 +25,7 @@ pwm.start(100) # 100 on
 
 device.display(img)
 
-time.sleep(2)
-
-pwm.ChangeDutyCycle(50)
-time.sleep(2)
-
-pwm.ChangeDutyCycle(25)
-time.sleep(2)
+time.sleep(5)
 
 pwm.ChangeDutyCycle(1)
-time.sleep(2)
+time.sleep(5)
