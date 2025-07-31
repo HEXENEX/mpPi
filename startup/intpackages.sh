6cad6e51695f67c1f0b167cc6eaf86cbb71e6521
Ryan Waltemate <waltemry@oregonstate.edu>
2025-07-31 00:10:50 -0700

    spell checked

--- startup/intpackages.sh
+++ startup/intpackages.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo -e "install packages...\n"
+echo -e "installing packages...\n"
 
 echo "updating sudo"
 sudo apt update
 echo -e "updating complete\n"
 
 echo "installing system packages"
-sudo apt install -y libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 libatlas-base-dev vlc python3-pip
+sudo apt install -y libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libatlas-base-dev vlc python3-pip
 echo -e "done\n"
 
 echo "installing pip packages"
-pip install pillow luma.lcd RPi.GPIO mutagen python-vlc
+pip3 install pillow luma.lcd RPi.GPIO mutagen python-vlc
 echo -e "done\n"
 
 echo "installing packages complete"
