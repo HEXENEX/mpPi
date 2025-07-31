787c0a8c3657d69dbac07fdfd9ceb812d49b4469
Ryan Waltemate <waltemry@oregonstate.edu>
2025-07-31 00:06:52 -0700

    edited install packages

--- startup/intpackages.sh
+++ startup/intpackages.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo "install packages..."
+echo -e "install packages...\n"
+
+echo "updating sudo"
+sudo apt update
+echo -e "updating complete\n"
+
+echo "installing system packages"
+sudo apt install -y libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 libatlas-base-dev vlc python3-pip
+echo -e "done\n"
 
 echo "installing pip packages"
 pip install pillow luma.lcd RPi.GPIO mutagen python-vlc
-echo "done"
-
-echo "installing system packages"
-sudo apt install libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 libatlas-base-dev vlc
-echo "done"
+echo -e "done\n"
 
 echo "installing packages complete"
