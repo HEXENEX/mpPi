d27c5258aba756054ae40111e01e073079ee24f8
Ryan Waltemate <waltemry@oregonstate.edu>
2025-07-31 00:17:29 -0700

    more tweaks

--- startup/intpackages.sh
+++ startup/intpackages.sh
@@ -7,11 +7,11 @@ sudo apt update
 echo -e "updating complete\n"
 
 echo "installing system packages"
-sudo apt install -y libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libatlas-base-dev vlc python3-pip
+sudo apt install -y libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libatlas-base-dev vlc python3-pillow python3-rpi.gpio
 echo -e "done\n"
 
 echo "installing pip packages"
-pip3 install pillow luma.lcd RPi.GPIO mutagen python-vlc
+pip3 install luma.lcd mutagen python-vlc
 echo -e "done\n"
 
 echo "installing packages complete"
