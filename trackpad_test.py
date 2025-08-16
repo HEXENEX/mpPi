65c69e2a9d4ff6fff879168c3550967cb3e72db2
Ryan W. <ryantheryan3@gmail.com>
2025-08-16 10:14:31 -0700

    test

--- trackpad_test.py
+++ trackpad_test.py
@@ -1,4 +1,3 @@
-import spidev
 import RPi.GPIO as GPIO
 import time
 
@@ -7,20 +6,13 @@ DR_PIN = 22
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DR_PIN, GPIO.IN)
 
-spi = spidev.SpiDev()
-spi.open(0, 1)          # bus 0, CE1 (GPIO7)
-spi.max_speed_hz = 1000000
-spi.mode = 0b01
-
+print("Watching DR pin on GPIO22. Touch the pad... (Ctrl+C to exit)")
 try:
-    print("Move your finger... Press Ctrl+C to exit")
     while True:
-        if GPIO.input(DR_PIN):
-            data = spi.readbytes(5)
-            print("Packet:", [f"{b:02X}" for b in data])
-        time.sleep(0.001)
+        val = GPIO.input(DR_PIN)
+        print("DR =", val)
+        time.sleep(0.05)  # 20 Hz refresh
 except KeyboardInterrupt:
     pass
 finally:
-    spi.close()
     GPIO.cleanup()
